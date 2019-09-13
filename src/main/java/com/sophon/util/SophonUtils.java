bb7cf4df9aba2065d47bf16aeb4bb1574a200a47
torocket <torocket@foxmail.com>
2019-09-14 05:33:00 +0800

    完善获取接口所有实现类的工具方法

--- src/main/java/com/sophon/util/SophonUtils.java
+++ src/main/java/com/sophon/util/SophonUtils.java
@@ -9,11 +9,14 @@ import com.sun.istack.internal.NotNull;
 import java.io.File;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * @Author tiansheng
  * @Date 2019/9/14 4:17
- * @Description 框架工具类
+ * @Description Sophon Logger for Java 日志框架工具类
  */
 public class SophonUtils {
 
@@ -32,15 +35,21 @@ public class SophonUtils {
                 String basePackage = target.getClassLoader().getResource("").getPath();
                 File[] files = new File(basePackage).listFiles();
                 // 存放class路径的list
-                ArrayList<String> classpath = Lists.newArrayList();
+                ArrayList<String> classpaths = Lists.newArrayList();
                 for (File file : files) {
-                    // 如果当前file目录是个对象就继续往下执行
+                    // 扫描项目编译后的所有类
                     if (file.isDirectory()) {
-                        listPackages(file.getName(),classpath);
+                        listPackages(file.getName(), classpaths);
                     }
                 }
-                for(String cn : classpath){
-                    System.out.println(cn);
+                // 获取所有类,然后判断是否是 target 接口的实现类
+                for (String classpath : classpaths) {
+                    Class<?> classObject = Class.forName(classpath);
+                    if(classObject.getSuperclass() == null) continue; // 判断该对象的父类是否为null
+                    Set<Class<?>> interfaces = new HashSet<>(Arrays.asList(classObject.getInterfaces()));
+                    if(interfaces.contains(target)){
+                        subclasses.add(Class.forName(classObject.getName()));
+                    }
                 }
             } else {
                 throw new ParamException("Class对象不是一个interface");
@@ -53,6 +62,7 @@ public class SophonUtils {
 
     /**
      * 获取编译后的所有类路径
+     *
      * @param basePackage
      * @param classes
      * @return
@@ -68,7 +78,7 @@ public class SophonUtils {
             } else {
                 // 如果不是一个目录,判断是不是以.class结尾的文件,如果不是则不作处理
                 String classpath = file.getName();
-                if(".class".equals(classpath.substring(classpath.length() - ".class".length()))) {
+                if (".class".equals(classpath.substring(classpath.length() - ".class".length()))) {
                     classes.add(basePackage + "." + classpath.replaceAll(".class", ""));
                 }
             }
@@ -76,7 +86,10 @@ public class SophonUtils {
     }
 
     public static void main(String[] args) {
-        getInterfaceImpl(SophonInit.class);
+        //
+        // 注意这个class对象是个接口,List中包含的是SophonInit接口的所有实现类
+        //
+        ArrayList<Class<?>> classes = getInterfaceImpl(SophonInit.class);
     }
 
 }
