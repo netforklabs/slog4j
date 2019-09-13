bfd4df5f5b4ac8c4cd82825fc71be32a6c6c4d2f
torocket <torocket@foxmail.com>
2019-09-14 03:50:04 +0800

    新增记录bug的文件

--- src/main/java/com/sophon/Example.java
+++ src/main/java/com/sophon/Example.java
@@ -1,9 +1,5 @@
 package com.sophon;
 
-import com.sophon.logger.Logger;
-
-import java.util.ArrayList;
-
 /**
  * @Author tiansheng
  * @Date 2019/9/12 3:40
@@ -13,24 +9,7 @@ public class Example {
 
     // private static final Logger log = LoggerFactory.getLogger(Example.class);
 
-
-
     public static void main(String[] args) {
-
-        for(int i=0; i<2000; i++){
-            Logger.info("hello slog4j info");
-            Logger.debug("hello slog4j debug");
-            Logger.error("hello slog4j error");
-            Logger.warn("hello slog4j warn");
-        }
-
-    }
-
-    public static void iosPush(){
-
-    }
-
-    public static void androidPush(){
 
     }
 
