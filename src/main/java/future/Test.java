43459e3faba9d7675fdff3481a110c717a772488
torocket <torocket@foxmail.com>
2019-09-26 03:40:18 +0800

    提交下研发代码，该代码可能会让slog4j变得焕然一新

---
+++ src/main/java/future/Test.java
@@ -0,0 +1,15 @@
+package future;
+
+/**
+ * @author tiansheng
+ * @version 1.0.0
+ * @date 2019/9/26 3:36
+ * @since 1.8
+ */
+public class Test {
+
+    public static void test(String a, int b, boolean c) {
+        System.out.println(a + "," + b + "," + c);
+    }
+
+}
