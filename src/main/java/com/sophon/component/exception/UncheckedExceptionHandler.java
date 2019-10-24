adfef912e2ab8287fecb010e125c511f48f3dcfa
torocket <spacecloud@foxmail.com>
2019-10-24 14:02:46 +0800

    update

--- src/main/java/com/sophon/component/exception/UncheckedExceptionHandler.java
+++ src/main/java/com/sophon/component/exception/UncheckedExceptionHandler.java
@@ -46,12 +46,22 @@ public class UncheckedExceptionHandler implements Thread.UncaughtExceptionHandle
 
     @Override
     public void init() {
-        currentGroup = Thread.currentThread().getThreadGroup();
-        // 当前活动线程
-        int activeCount = currentGroup.activeCount();
-        Thread[] group = new Thread[activeCount];
-        currentGroup.enumerate(group);
-        for (Thread thread : group) {
+        ThreadGroup group = Thread.currentThread().getThreadGroup();
+        ThreadGroup topGroup = group;
+        // 遍历线程组树，获取根线程组
+        while (group != null) {
+            topGroup = group;
+            group = group.getParent();
+        }
+        // 激活的线程数加倍
+        int estimatedSize = topGroup.activeCount() * 2;
+        Thread[] slackList = new Thread[estimatedSize];
+        // 获取根线程组的所有线程
+        int actualSize = topGroup.enumerate(slackList);
+        // copy into a list that is the exact size
+        Thread[] list = new Thread[actualSize];
+        System.arraycopy(slackList, 0, list, 0, actualSize);
+        for (Thread thread : list) {
             thread.setUncaughtExceptionHandler(new UncheckedExceptionHandler());
         }
     }
